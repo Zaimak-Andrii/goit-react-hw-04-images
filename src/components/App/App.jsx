d47d4fff40b066e3daefd0b6cbb70ab058490e87
Andrii Zaimak <xiveres@MacBook-Air-Andrii.local>
2022-12-31 17:56:16 +0200

    Change Class component to function component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,19 @@
+import { useState } from 'react';
 import ImageGallery from 'components/ImageGallery';
 import Searchbar from 'components/Searchbar';
-import { Component } from 'react';
 import css from './App.module.css';
 
-export default class App extends Component {
-  state = {
-    searchQuery: '',
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchHandler = value => {
+    setSearchQuery(value);
   };
 
-  searchHandler = value => {
-    this.setState({ searchQuery: value });
-  };
-
-  render() {
-    return (
-      <div className={css.app}>
-        <Searchbar onSubmit={this.searchHandler} />
-        <ImageGallery searchQuery={this.state.searchQuery} />
-      </div>
-    );
-  }
+  return (
+    <div className={css.app}>
+      <Searchbar onSubmit={searchHandler} />
+      <ImageGallery searchQuery={searchQuery} />
+    </div>
+  );
 }
